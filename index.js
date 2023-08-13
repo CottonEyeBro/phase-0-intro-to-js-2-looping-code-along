4950ce5d8f05a03944984f3dc2b15c3d14eb7af2
Cooper <55swimmer55@gmail.com>
2023-08-13 15:09:58 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const cardList = [];
+    for (let i = 0; i < names.length; i++) {
+        const cardMessage = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        cardList.push(cardMessage);
+    }
+    return cardList;
+}
+
+function countDown(integer) {
+    let i = 0
+    while (i <= integer) {
+        let subtractInteger = integer - i;
+        console.log(subtractInteger);
+        i++;
+    }
+}
